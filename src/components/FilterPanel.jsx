8e60ab76ccaf466ba0f58649162b4f446c2f71b7
Borys Mch <borys.mch@gmail.com>
2023-05-20 09:04:33 +0300

    Added adding badge on panel per click

--- src/components/FilterPanel.jsx
+++ src/components/FilterPanel.jsx
@@ -1,22 +1,29 @@
-import { Badge } from 'UI/Badge';
-import { Card } from 'UI/Card';
-import { Stack } from 'UI/Stack';
+import { useSelector, useDispatch } from "react-redux";
 
+import { Badge } from "UI/Badge";
+import { Card } from "UI/Card";
+import { Stack } from "UI/Stack";
+
+import { selectFilters } from "store/filters/filterSelectors";
 
 const FilterPanel = () => {
+  const currentFilters = useSelector(selectFilters);
+
   return (
     <Card className="filter-panel">
       <div className="filter-panel-wrapper">
         <Stack>
-          <Badge variant="clearable">Frontend</Badge>
-          <Badge variant="clearable">Backend</Badge>
-          <Badge variant="clearable">React</Badge>
+          {currentFilters.map((filter) => (
+            <Badge key={filter} variant="clearable">
+              {filter}
+            </Badge>
+          ))}
         </Stack>
 
-        <button className='link'>Clear</button>
+        <button className="link">Clear</button>
       </div>
     </Card>
-  )
-}
+  );
+};
 
-export {FilterPanel};
+export { FilterPanel };
